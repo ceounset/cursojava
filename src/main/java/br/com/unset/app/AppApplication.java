aaa7f9211bdbbfe3431f233f9fd90043bf416aaa
ceounset <ceo@unset.com.br>
2021-06-19 15:49:37 -0300

    Projeto criado

---
+++ src/main/java/br/com/unset/app/AppApplication.java
@@ -0,0 +1,13 @@
+package br.com.unset.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
