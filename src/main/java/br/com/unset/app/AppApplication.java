09d137b4b3e3513445e3b16ade7efdac1b0ddefb
ceounset <ceo@unset.com.br>
2021-06-21 13:07:48 -0300

    Criação de operação de instanciação

--- src/main/java/br/com/unset/app/AppApplication.java
+++ src/main/java/br/com/unset/app/AppApplication.java
@@ -1,13 +1,35 @@
 package br.com.unset.app;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.unset.app.domain.Categoria;
+import br.com.unset.app.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Teste");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+	}
+
+	
 
 }
