33d34cadf761782e2d93f3c3dcb651ab2fd5c285
ceounset <ceo@unset.com.br>
2021-06-24 13:47:00 -0300

    Estado e Cidade

--- src/main/java/br/com/unset/app/AppApplication.java
+++ src/main/java/br/com/unset/app/AppApplication.java
@@ -1,5 +1,6 @@
 package br.com.unset.app;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.unset.app.domain.Categoria;
+import br.com.unset.app.domain.Cidade;
+import br.com.unset.app.domain.Estado;
 import br.com.unset.app.domain.Produto;
 import br.com.unset.app.repositories.CategoriaRepository;
+import br.com.unset.app.repositories.CidadeRepository;
+import br.com.unset.app.repositories.EstadoRepository;
 import br.com.unset.app.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +24,10 @@ public class AppApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
@@ -34,19 +43,28 @@ public class AppApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 300.50);
 		Produto p3 = new Produto(null, "Mouse", 50.00);
 
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
-	}
+		Estado e1 = new Estado(null, "Mato Grosso");
+		Estado e2 = new Estado(null, "São Paulo");
 
-	
+		Cidade c1 = new Cidade(null, "Nova Xavantina", e1);
+		Cidade c2 = new Cidade(null, "Campinas", e2);
+		Cidade c3 = new Cidade(null, "Água Boa", e1);
+
+		e1.getCidades().addAll(Arrays.asList(c1, c3));
+		e2.getCidades().addAll(Arrays.asList(c2));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(e1, e2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+	}
 
 }
